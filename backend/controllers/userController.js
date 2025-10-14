247d2781e3a7dd50a73f416d22f4063a55ccc744
bin0120120 <150657888+bin0120120@users.noreply.github.com>
2025-10-14 18:03:31 +0700

    1

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import db from "../db/conectdb.js";
 
 // Lấy tất cả user
 export const getUsers = (req, res) => {
-  db.query("SELECT * FROM user", (err, results) => {
+  db.query("SELECT * FROM tai_khoan", (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json(results);
   });
@@ -11,19 +11,19 @@ export const getUsers = (req, res) => {
 // Lấy 1 user theo ID
 export const getUserById = (req, res) => {
   const { id } = req.params;
-  db.query("SELECT * FROM user WHERE id = ?", [id], (err, results) => {
+  db.query("SELECT * FROM tai_khoan WHERE id = ?", [id], (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
     if (results.length === 0) return res.status(404).json({ message: "Không tìm thấy user" });
     res.json(results[0]);
   });
 };
 
-// Thêm user
+//  Thêm user
 export const createUser = (req, res) => {
   const { ten, email, mat_khau, dien_thoai, dia_chi, vai_tro, trang_thai, anh } = req.body;
 
   const sql = `
-    INSERT INTO user (ten, email, mat_khau, dien_thoai, dia_chi, vai_tro, trang_thai, anh)
+    INSERT INTO tai_khoan (ten, email, mat_khau, dien_thoai, dia_chi, vai_tro, trang_thai, anh)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?)
   `;
   db.query(sql, [ten, email, mat_khau, dien_thoai, dia_chi, vai_tro, trang_thai, anh], (err, result) => {
@@ -32,26 +32,26 @@ export const createUser = (req, res) => {
   });
 };
 
-// Sửa user
+//  Sửa user
 export const updateUser = (req, res) => {
   const { id } = req.params;
   const { ten, email, mat_khau, dien_thoai, dia_chi, vai_tro, trang_thai, anh } = req.body;
 
   const sql = `
-    UPDATE user
+    UPDATE tai_khoan
     SET ten=?, email=?, mat_khau=?, dien_thoai=?, dia_chi=?, vai_tro=?, trang_thai=?, anh=?
     WHERE id=?
   `;
-  db.query(sql, [ten, email, mat_khau, dien_thoai, dia_chi, vai_tro, trang_thai, anh, id], (err, result) => {
+  db.query(sql, [ten, email, mat_khau, dien_thoai, dia_chi, vai_tro, trang_thai, anh, id], (err) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json({ message: "Cập nhật user thành công" });
   });
 };
 
-// Xóa user
+//  Xóa user
 export const deleteUser = (req, res) => {
   const { id } = req.params;
-  db.query("DELETE FROM user WHERE id = ?", [id], (err) => {
+  db.query("DELETE FROM tai_khoan WHERE id = ?", [id], (err) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json({ message: "Xóa user thành công" });
   });
